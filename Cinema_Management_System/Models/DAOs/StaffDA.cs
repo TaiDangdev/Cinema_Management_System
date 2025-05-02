fd34400d781daab6319c12c375a1b42b7b484a11
Truong Nhat Nguyen <tnhatnguyen.dev2805@gmail.com>
2025-05-02 09:12:32 +0700

    update Staff DTO,DA

---
+++ Cinema_Management_System/Models/DAOs/StaffDA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Management_System.Models.DAOs
+{
+    public class StaffDA
+    {
+        ConnectDataContext connect = new ConnectDataContext();
+
+        private static StaffDA instance;
+
+        public static StaffDA Instance
+        {
+            get { if (StaffDA.instance == null) StaffDA.instance = new StaffDA(); return StaffDA.instance; }
+            set { StaffDA.instance = value; }
+        }
+
+        private StaffDA() { }
+
+
+    }
+}
---
+++ Cinema_Management_System/Models/DTOs/StaffDTO.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Cinema_Management_System.Models.DTOs
+{
+    public class StaffDTO
+    {
+        public int Id { get; set; }
+        public string IdFormat { get; set; }
+        public string FullName { get; set; }
+        public string Birth { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Salary { get; set; }
+        public string Role { get; set; }
+        public string NgayVaoLam { get; set; }
+        public BitmapImage ImageSource { get; set; }
+
+        //phục vụ edit
+        public StaffDTO(int id, string fullName, string birth, string gender, string email, string phoneNumber, int salary, string role, string ngayVL)
+        {
+            Id = id;
+            FullName = fullName;
+            Birth = birth;
+            Gender = gender;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Salary = salary;
+            Role = role;
+            NgayVaoLam = ngayVL;
+            IdFormat = StaffDA.formatID(Id);
+        }
+
+
+        //phục vụ việc thêm 1 staff
+        public StaffDTO(string fullName, string birth, string gender, string email, string phoneNumber, int salary, string role, string ngayVL)
+        {
+            FullName = fullName;
+            Birth = birth;
+            Gender = gender;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Salary = salary;
+            Role = role;
+            NgayVaoLam = ngayVL;
+        }
+
+
+        //phục vụ phần information và lấy data
+        public StaffDTO(int id, string fullName, string birth, string gender, string email, string phoneNumber, int salary, string role, string ngayVL, BitmapImage imageSource)
+        {
+            Id = id;
+            FullName = fullName;
+            Birth = birth;
+            Gender = gender;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Salary = salary;
+            Role = role;
+            NgayVaoLam = ngayVL;
+            //IdFormat = StaffDA.formatID(Id);
+            ImageSource = imageSource;
+        }
+    }
+}
